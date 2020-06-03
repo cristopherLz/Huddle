3f432dcf3f07eb7965b2bfee3578b4e66c4a471f
cristopherLz <zerrillo.c@gmail.com>
2020-06-03 13:53:57 -0500

    Animate Modal
    Animacion del cuadro de registro

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+    var open = document.getElementById('open')
+    var moda = document.getElementById('modal')
+    var content = document.getElementById('content')
+
+open.addEventListener('click',
+    function(){
+        moda.classList.add('active');
+        content.style.opacity='1';
+        
+});
+
